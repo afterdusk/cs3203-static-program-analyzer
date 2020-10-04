4a3ffce1e65f3cefbfafbf392c7043fe72e6d5aa
Liang Jun <au.liangjun@gmail.com>
2020-10-04 16:17:59 +0800

    Add parse and evaluate implementation

--- Team20/Code20/Gui/GUIWrapper.cpp
+++ Team20/Code20/Gui/GUIWrapper.cpp
@@ -23,18 +23,27 @@ std::string GUIWrapper::read(std::string filename) {
 
 // method for parsing the SIMPLE source
 void GUIWrapper::parse(std::string filename) {
-  // call your parser to do the parsing
-  std::cout << "parsed " << filename << std::endl;
-  // ...rest of your code...
+  try {
+    Parser parser(read(filename), pkb.getTables());
+    parser.parse();
+    std::cout << "parsed " << filename << std::endl;
+  } catch (ParseException &p) {
+    std::cout << p.what() << std::endl;
+  }
 }
 
 // method to evaluating a query
 void GUIWrapper::evaluate(std::string query, std::list<std::string> &results) {
-  // call your evaluator to evaluate the query here
-  // ...code to evaluate query...
-
   std::cout << "query=  " << query << std::endl;
-  results.push_back(query + "\nQuery result: ...");
-  // store the answers to the query in the results list (it is initially empty)
-  // each result must be a string.
+  try {
+    Pql::evaluate(Pql::parse(Pql::lex(query)), pkb.getQueryInterface(),
+                  results);
+  } catch (...) {
+    results.clear();
+  }
+  std::string separator;
+  for (auto &result : results) {
+    std::cout << separator << result;
+    separator = ",";
+  }
 }
